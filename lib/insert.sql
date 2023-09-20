f3df808d0c535c15a2ec1ace1431382f6f56b710
Vanessa Jesik <vajesik@gmail.com>
2023-09-20 16:48:19 -0600

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,127 @@
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Mr. Chocolate",
+5,
+"M",
+"chocolate",
+"hungry",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Rowdy",
+2,
+"M",
+"cinnamon",
+"rowdy",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Tabitha",
+12,
+"F",
+"brown",
+"sleepy",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Serbeant Brown",
+6,
+"M",
+"brown",
+"bossy",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Melissa",
+8,
+"F",
+"black",
+"quick",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Grinch",
+12,
+"M",
+"cinnamon",
+"grumpy",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+"Wendy",
+1,
+"F",
+"white",
+"rude",
+True
+);
+
+INSERT INTO bears (
+name, 
+age, 
+sex, 
+color, 
+temperament,
+alive
+) VALUES (
+Null,
+7,
+"M",
+"black",
+"pesky",
+True
+);
